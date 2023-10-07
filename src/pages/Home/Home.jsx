637e56a5ff2d08c12903c35350c9fbd78dacd1e6
Vishnu Kumar <vermaji1891@gmail.com>
2023-10-07 11:42:07 +0530

    multiple rerquest done with axios

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,19 +6,21 @@ import { searchMovie } from "../../apis/omdb";
 
 function Home () {
 
-    const [movies, setMovies] = useState([]);
+    const [MovieList, setMovieList] = useState([]);
 
     
 
-    async function downloadDefaultMovies (movieName) {
-        const response = await axios.get(searchMovie(movieName))
-        setMovies([...response.data.Search])
+    async function downloadDefaultMovies (...args) {
+        const urls = args.map((name) => searchMovie(name))
+        const response = await axios.all((urls.map(url => axios.get(url))))
+        const movies = response.map((movieResponse) => movieResponse.data.Search)
+        setMovieList([].concat(...movies))
     }
 
 
 
     useEffect(() => {
-        downloadDefaultMovies("harry")
+        downloadDefaultMovies("harry", "batman", "avengers", "superman")
     }, [])
     return (
         <>
@@ -28,7 +30,7 @@ function Home () {
             
             <div className="movie-card-parent">
            
-            {movies.map((movie) => 
+            {MovieList.length > 0 && MovieList.map((movie) => 
                 <MovieCard  key={movie.omdbId} Title={movie.Title} Year={movie.Year} Poster={movie.Poster}/>
             )}
             </div>
